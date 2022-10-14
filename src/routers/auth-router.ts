236b0cfdff55406a4744df0cfc712a989c9ae676
Bucktabu <stanislaworonin@yandex.ru>
2022-10-14 18:31:35 +0300

    add mapping

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -9,8 +9,8 @@ export const authRouter = Router({})
 
 authRouter.post('/login',
     ...authRouterValidationMiddleware,
-    async (req: RequestWithBody<UserTryLogIn>, res: Response) => {
-        const user = await usersService.checkCredential(req.body.loginOrEmail, req.body.password)
+    async (req: Request, res: Response) => {
+        const user = await usersService.checkCredential(req.body.login, req.body.password)
 
         if (!user) {
             return res.sendStatus(401)
